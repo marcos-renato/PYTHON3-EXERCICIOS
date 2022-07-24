73441c17f13dc7596fc407ef10d79d1b0e8d7cc6
Marcos Renato Pinto Vicente <106030186+marcos-renato@users.noreply.github.com>
2022-07-24 17:49:57 -0300

    segunda atualização

--- aula4_tipos_primitivos_de_dados.py
+++ aula4_tipos_primitivos_de_dados.py
@@ -5,3 +5,8 @@ int - inteiro
 float - real/ponto flutuante
 boll - booleano/logico
 """
+print('Luiz',type('Luiz'))
+print(10,type(10))
+print(25.23,type(25.23))
+print('l'=='l',type('l'=='l'))
+print('L'=='l',type('L'=='l'))
