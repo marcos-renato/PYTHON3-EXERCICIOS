1974f023471f4aecc1d3fc3d96cf38fb4dc9ce57
Marcos Renato Pinto Vicente <106030186+marcos-renato@users.noreply.github.com>
2022-07-24 17:39:58 -0300

    aula 1 a aula 4

---
+++ aula1_comenentarios_em_python.py
@@ -0,0 +1,7 @@
+## o cerquilha é utilizado para fazer comentario de linha unica.
+print('A cerquilha se for colocada atráz de um comando e necessário dá dois espaços')  ## exemplo de comentario atraz de um codigo
+print('Podemos comentar utilizando notação de documentos ')
+"""
+  Exemplo de de anotações de mais de uma linha
+  neste exemplo tem duas linhas
+"""
---
+++ aula4_tipos_primitivos_de_dados.py
@@ -0,0 +1,7 @@
+"""
+TIPOS PRIMITIVOS DE DADOS:
+str - string
+int - inteiro
+float - real/ponto flutuante
+boll - booleano/logico
+"""
